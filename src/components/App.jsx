900c3c7484822e46d5d0bfd70341388178bab189
andriy-h80 <andrey.hanzel@gmail.com>
2023-05-04 08:50:51 +0300

    all steps done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,55 @@
-import { Container } from './App.styled';
-import Feedback from './Feedback';
+import React, { Component } from 'react';
+import { Container } from './Container/Container';
+import { Section } from './Section/Section';
+import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
+import { Statistics } from './Statistics/Statistics';
 
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0
+  }
 
-export const App = () => {
-  return (
-    <Container>
-     
-      <Feedback />
+  leaveFeedback = (event) => {
+    this.setState({ [event]: this.state[event] + 1 });
+  };
 
-    </Container>
-  );
+  countTotalFeedback = () => {
+    const { good, neutral, bad } = this.state;
+    const total = good + neutral + bad;
+    return total;
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    const { good, neutral, bad } = this.state;
+    const total = good + neutral + bad;
+    if (total === 0) {
+        return 0;
+      }
+    return Math.round((good / total) * 100);
+  };
+  
+  render() {
+    const { good, neutral, bad } = this.state;
+    return (
+      <Container>
+        <Section title="Please leave feedback">
+          <FeedbackOptions
+            options={Object.keys(this.state)}
+            onLeaveFeedback={this.leaveFeedback}
+          />
+        </Section>
+        <Section title="Statistics">
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={this.countTotalFeedback()}
+            positivePercentage={this.countPositiveFeedbackPercentage()}
+          ></Statistics>
+        </Section>
+      </Container>
+    );
+  } 
 };
--- src/components/App.styled.jsx
+++
@@ -1,9 +0,0 @@
-import styled from '@emotion/styled';
-
-export const Container = styled.div`
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    fontSize: 40;
-`;
